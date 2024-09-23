9bfb3228be3d6f6e99ffab1bbb7102e25acef530
maya-nachiappan <maya.n102304@gmail.com>
2024-09-23 11:06:47 -0700

    takes user input for amount of rows and uses two for loops to print stars

--- print a triangle/main.cpp
+++ print a triangle/main.cpp
@@ -4,11 +4,22 @@
 //
 //  Created by maya nachiappan on 9/23/24.
 //
+// program to take user input for number of rows and print a triangle with *
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    int row;
+    cout << "enter the number of rows you would like your triangle to have:" << endl;
+    cin >> row;
+    for (int x=0; x<row; x++)
+    {
+        for (int y=0; y<x; y++)
+        {
+            cout <<"*";
+        }
+        cout << endl;
+    }
     return 0;
 }
